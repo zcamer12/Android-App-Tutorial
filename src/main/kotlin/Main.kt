050200ae6ceb1e00c2b8afbd18eb0a9dbb9f440e
GustPhoenix <jailbird174@gmail.com>
2023-09-07 10:21:43 -0800

    first commit for app

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(args: Array<String>) {
+
+    val wordArray1 = arrayOf("24/7", "multi-tier", "B-to-B", "dynamic")
+
+    val wordArray2 = arrayOf("empowered", "leveraged", "aligned", "tame")
+
+    val wordArray3 = arrayOf("process", "paradigm", "solution", "portability")
+
+
+    val arraySize1 = wordArray1.size
+
+    val arraySize2 = wordArray2.size
+
+    val arraySize3 = wordArray3.size
+
+    val rand1 = (Math.random() * arraySize1).toInt()
+
+    val rand2 = (Math.random() * arraySize2).toInt()
+
+    val rand3 = (Math.random() * arraySize3).toInt()
+
+    val phrase = "${wordArray1[rand1]} ${wordArray2[rand2]} ${wordArray3[rand3]}"
+
+    println(phrase)
+}
